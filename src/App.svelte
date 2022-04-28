89dc0018db2f23e74cd2019f1e4c15a243dd6970
Hollister009 <reactive.andrew@yahoo.com.ua>
2022-04-28 21:05:43 +0300

    Category & Directory components

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,12 @@
 <script>
-  import logo from './assets/svelte.png'
+  import Directory from './components/Directory.svelte';
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Directory />
 </main>
 
-<style lang="scss">
+<style>
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -27,36 +16,5 @@
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
   }
 </style>
---
+++ src/components/Directory.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Category from './Category.svelte';
+  import { CATEGORIES } from '../constants';
+</script>
+
+<div class="categories-container">
+  {#each CATEGORIES as { title, imageUrl }}
+    <Category {title} {imageUrl} />
+  {/each}
+</div>
+
+<style>
+  .categories-container {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+</style>
